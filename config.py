fa652ca478d134512352c40a6cfd035f5330dbb4
Cao Long <65386186+123olala@users.noreply.github.com>
2021-09-27 21:45:40 +0700

    create link path

---
+++ config.py
@@ -0,0 +1,2 @@
+COMPANY = '../financial-dasboard/data/Singapore ICB.xlsx'
+MARKETCAP_PRICE = '../financial-dasboard/data/Singapore - Price - Market Cap.xlsx'
--- process.py
+++ process.py
@@ -1,1 +1,4 @@
 #use to process data
+import pandas as pd
+import config
+print(pd.read_excel(config.COMPANY))
