2be2d8da20b65fc1a3a862e3ed9c5267696e587f
Cao Long <caolong785@gmail.com>
2021-10-09 17:03:42 +0700

    Update process.py

--- process.py
+++ process.py
@@ -83,6 +83,7 @@ def full_marketcap_price(data='market'):
         #Delete Exchange-traded fund and company was not listed in Singagore
         df = df[~df['Name'].str.contains("\\(SES")]
         df = df[~df['Name'].str.contains("ETF")]
+        df.reset_index(drop=True,inplace=True)
         #Correct data of Delist or Suspended company 
         delist_time = [name.split('.')[-1] for name in df.loc[df['Status Name'].str.contains('DELIST')]['Status Name']]
         delist_time = [date_available(dt.strftime(dt.strptime(i,'%d/%m/%y'),'%Y-%m-%d')) for i in delist_time]
